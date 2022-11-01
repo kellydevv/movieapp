104cb8fa006da53c7537f68d83ae34f54443928b
정현진 <jhj9328@hanmail.net>
2022-11-01 15:27:32 +0900

    route path 수정

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Detail />} />
       </Routes>
     </Router>
   );
